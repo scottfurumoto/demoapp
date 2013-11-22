b3aafe13ca04fdc327b60cb20ee3125b2fd9dee2
Scott Furumoto <sfurum@aol.com>
2013-11-22 02:32:38 +0000

    First commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131115023523) do
+ActiveRecord::Schema.define(version: 20131122021639) do
 
   create_table "users", force: true do |t|
     t.string   "name"
-    t.string   "string"
-    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
